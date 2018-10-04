09eb5d9ca4ae5c1c99bfdbf4a6455c217acf39df
tiemenv <tiemen.vermote@gmail.com>
2018-10-04 14:38:04 +0200

    Adding source files

---
+++ protected/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Labs Web Security</title>
+    <link href="../assets/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/screen.css" rel="stylesheet" type="text/css"/>
+  </head>
+  <body id="top" class="protected"><div class="site-wrapper">
+  <div class="site-wrapper-inner">
+    <div class="cover-container">
+      <div class="masthead clearfix">
+        <div class="inner">
+          
+        </div>
+      </div><br>
+	  </div>
+	  <div class="inner cover">
+        <h1 class="cover-heading">Welcome to protected area!</h1>
+        <p class="lead cover-copy">Lab 2 - Authentication in Apache</p>
+	</div>
+  </body>
+</html>
